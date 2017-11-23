e0a8729be856e5662116f4740aec1f90b290ccb0
wh1tecat_mac <wh1tecat@wh1tecat-mac.local>
2017-11-23 18:38:24 +0900

    just worked release

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 const ffmpeg = require("fluent-ffmpeg");
 const ism4a = require("is-m4a");
-const btoa = require("btoa");
 const stream = require("stream");
 
-module.exports = buffer_m4a => new Promise((resolve, reject) => {
+module.exports = buffer_m4a => new Promise(async (resolve, reject) => {
 	if (!buffer_m4a) {
 		reject(new Error("You must input m4a binary file."));
 	}
@@ -13,12 +12,23 @@ module.exports = buffer_m4a => new Promise((resolve, reject) => {
 		reject(new Error("this is not m4a files."));
 	}
 	try {
-		const bufferStream = new stream.PassThrough();
-		const outBufferStream = new stream.PassThrough();
-		bufferStream.end(buffer_m4a);
-		bufferStream.pipe();
-		ffmpeg(bufferStream)
-			.input()
+		const Writer = new stream.Writable();
+		Writer.data = [];
+		Writer.data.push(new Buffer("524946464618010057415645666D74201000000001000100803E0000007D0000020010004C4953541A000000494E464F495346540E0000004C61766635372E37312E313030006461746100180100","hex"));
+		Writer._write = (chunk, chunk2, callback) => {
+			Writer.data.push(chunk);
+			callback();
+		};
+
+		const inputstream = (buffer) => {
+			const istream = new stream.Duplex();
+			istream.push(buffer);
+			istream.push(null);
+			return stream;
+		};
+
+		await ffmpeg()
+			.input(inputstream(buffer_m4a))
 			.outputOptions([
 				"-f s16le",
 				"-acodec pcm_s16le",
@@ -27,8 +37,16 @@ module.exports = buffer_m4a => new Promise((resolve, reject) => {
 				"-ar 16k",
 				"-map_metadata -1"
 			])
-			.pipe(outBufferStream)
-			.on("end", () => resolve(outBufferStream));
+			.on("error", (error, stdout, stderr) => {
+				console.log(error);
+				console.log(stdout);
+				console.log(stderr);
+			})
+			.on("end", (stdout) => {
+				Writer.data = Buffer.concat(Writer.data);
+				resolve(Writer.data);
+			})
+			.stream(Writer);
 	} catch (e) {
 		reject(e);
 	}
